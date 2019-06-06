9576738eecf08035738e7ea4ef7b99495e38d43f
Chris Piccaro <chrispiccaro18@gmail.com>
2019-06-05 17:06:15 -0700

    graveyard post and test

---
+++ test/e2e/graveyard.test.js
@@ -0,0 +1,36 @@
+require('../connect-db');
+const request = require('supertest');
+const app = require('../../lib/app');
+
+describe('graveyard routes', () => {
+  const testGraveyard = {
+    name: 'test graveyard',
+    location: {
+      lat: '47.1443',
+      lng: '-122.1408',
+      city: 'Prairie Ridge',
+      state: 'WA'
+    },
+    totalGraves: 100,
+    occupiedGraves: 20
+  };
+  it('can create a graveyard', async() => {
+    const res = await request(app)
+      .post('/api/v1/graveyards')
+      .send(testGraveyard);
+    
+    expect(res.body).toEqual({
+      _id: expect.any(String),
+      __v: 0,
+      name: 'test graveyard',
+      location: {
+        lat: '47.1443',
+        lng: '-122.1408',
+        city: 'Prairie Ridge',
+        state: 'WA'
+      },
+      totalGraves: 100,
+      occupiedGraves: 20
+    });
+  });
+});
