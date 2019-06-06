00054eab1c1cb15a487bed4a72004eb8bdd0a801
Chris Piccaro <chrispiccaro18@gmail.com>
2019-06-06 10:25:20 -0700

    get a graveyard by id

--- test/e2e/graveyards.test.js
+++ test/e2e/graveyards.test.js
@@ -57,4 +57,61 @@ describe('graveyard routes', () => {
       occupiedGraves: 20
     }]);
   });
+  
+  it('can get a graveyard by id with all graves', async() => {
+    const createdGraveyard = await request(app)
+      .post('/api/v1/graveyards')
+      .send(testGraveyard);
+
+    const graveyardId = createdGraveyard.body._id;
+    const testGrave1 = {
+      occupied: true,
+      occupant: 'test corpse 1',
+      graveyard: graveyardId
+    };
+    
+    const testGrave2 = {
+      occupied: false,
+      graveyard: graveyardId
+    };
+
+    await request(app)
+      .post('/api/v1/graves')
+      .send(testGrave1);
+    await request(app)
+      .post('/api/v1/graves')
+      .send(testGrave2);
+
+    const res = await request(app)
+      .get(`/api/v1/graveyards/${graveyardId}`);
+      
+    expect(res.body).toEqual({
+      _id: expect.any(String),
+      __v: 0,
+      name: 'test graveyard',
+      location: {
+        lat: '47.1443',
+        lng: '-122.1408',
+        city: 'Prairie Ridge',
+        state: 'WA'
+      },
+      totalGraves: 100,
+      occupiedGraves: 20,
+      graves: [
+        {
+          _id: expect.any(String),
+          __v: 0,
+          occupied: true,
+          occupant: 'test corpse 1',
+          graveyard: graveyardId.toString()
+        },
+        {
+          _id: expect.any(String),
+          __v: 0,
+          occupied: false,
+          graveyard: graveyardId.toString()
+        }
+      ]
+    });
+  });
 });
