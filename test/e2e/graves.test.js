7c2104bcf992781bca8d4c5d49d684ef833c83ee
Chris Piccaro <chrispiccaro18@gmail.com>
2019-06-06 09:59:42 -0700

    graves routes with tests

---
+++ test/e2e/graves.test.js
@@ -0,0 +1,44 @@
+require('../connect-db');
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../../lib/app');
+
+describe('grave routes', () => {
+  const graveyardId = new mongoose.Types.ObjectId;
+  const testGrave = {
+    occupied: true,
+    occupant: 'test corpse',
+    graveyard: graveyardId
+  };
+
+  it('can create a grave', async() => {
+    const res = await request(app)
+      .post('/api/v1/graves')
+      .send(testGrave);
+    
+    expect(res.body).toEqual({
+      _id: expect.any(String),
+      __v: 0,
+      occupied: true,
+      occupant: 'test corpse',
+      graveyard: graveyardId.toString()
+    });
+  });
+  
+  it('can get all graves', async() => {
+    await request(app)
+      .post('/api/v1/graves')
+      .send(testGrave);
+      
+    const res = await request(app)
+      .get('/api/v1/graves');
+
+    expect(res.body).toEqual([{
+      _id: expect.any(String),
+      __v: 0,
+      occupied: true,
+      occupant: 'test corpse',
+      graveyard: graveyardId.toString()
+    }]);
+  });
+});
