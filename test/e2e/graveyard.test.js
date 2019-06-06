3a07c3462c8a14efd2e8270e176b1d1c33c970c8
Chris Piccaro <chrispiccaro18@gmail.com>
2019-06-05 17:26:15 -0700

    get all graveyards and test

--- test/e2e/graveyard.test.js
+++ test/e2e/graveyard.test.js
@@ -14,6 +14,7 @@ describe('graveyard routes', () => {
     totalGraves: 100,
     occupiedGraves: 20
   };
+
   it('can create a graveyard', async() => {
     const res = await request(app)
       .post('/api/v1/graveyards')
@@ -32,5 +33,28 @@ describe('graveyard routes', () => {
       totalGraves: 100,
       occupiedGraves: 20
     });
+  });
+  
+  it('can get all graveyards', async() => {
+    await request(app)
+      .post('/api/v1/graveyards')
+      .send(testGraveyard);
+      
+    const res = await request(app)
+      .get('/api/v1/graveyards');
+    
+    expect(res.body).toEqual([{
+      _id: expect.any(String),
+      __v: 0,
+      name: 'test graveyard',
+      location: {
+        lat: '47.1443',
+        lng: '-122.1408',
+        city: 'Prairie Ridge',
+        state: 'WA'
+      },
+      totalGraves: 100,
+      occupiedGraves: 20
+    }]);
   });
 });
